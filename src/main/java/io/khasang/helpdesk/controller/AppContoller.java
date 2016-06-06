102ea7ebd3af2f774fff68c197464d35756a9dea
Kuznetsov Sergey <azon.sk@gmail.com>
2016-06-06 20:53:12 +0300

    ...

--- src/main/java/io/khasang/helpdesk/controller/AppContoller.java
+++ src/main/java/io/khasang/helpdesk/controller/AppContoller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class AppContoller {
 
+    //4to to sdelal
     @RequestMapping("/")
     public String hello(Model model){
         model.addAttribute("hello", "Welcome to Help Desk program!");
