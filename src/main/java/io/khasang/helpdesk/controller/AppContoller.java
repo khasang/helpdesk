46b3dbf9049adb3fed3803fc0ab3cb6186128bd7
Alexander <2121alexander2121@gmail.com>
2016-06-12 20:16:36 +0300

    add index, admin, task, desk pages

--- src/main/java/io/khasang/helpdesk/controller/AppContoller.java
+++ src/main/java/io/khasang/helpdesk/controller/AppContoller.java
@@ -10,6 +10,24 @@ public class AppContoller {
     @RequestMapping("/")
     public String hello(Model model){
         model.addAttribute("hello", "Welcome to Help Desk program!");
-        return "hello";
+        return "index";
+    }
+
+    @RequestMapping("/admin")
+    public String admin(Model model){
+        model.addAttribute("hello", "Admin page!");
+        return "admin";
+    }
+
+    @RequestMapping("/desk")
+    public String desk(Model model){
+        model.addAttribute("hello", "Desk page!");
+        return "desk";
+    }
+
+    @RequestMapping("/desk/task")
+    public String task(Model model){
+        model.addAttribute("hello", "Task page!");
+        return "task";
     }
 }
