11bbf42b2bf7bd6a78107d822683129707bf95e7
mrak <deolands@gmail.com>
2016-06-14 13:26:55 +0300

    remove class AppController.java

--- src/main/java/io/khasang/helpdesk/controller/AppController.java
+++ src/main/java/io/khasang/helpdesk/controller/AppController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class AppContoller {
+public class AppController {
 
     @RequestMapping("/")
     public String hello(Model model){
