789883431bac31a9e682bcf08ac0349b37d20266
Kuznetsov Sergey <azon.sk@gmail.com>
2016-06-15 20:46:46 +0300

    add spring test and spring jdbc config

--- src/main/java/io/khasang/helpdesk/config/AppContext.java
+++ src/main/java/io/khasang/helpdesk/config/AppContext.java
@@ -1,15 +1,47 @@
 package io.khasang.helpdesk.config;
 
 import io.khasang.helpdesk.model.Message;
+import io.khasang.helpdesk.model.Temp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
+@PropertySource("classpath:util.properties")
 public class AppContext {
+
+    @Autowired
+    Environment environment;
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource());
+        return jdbcTemplate;
+    }
+
+    @Bean
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(environment.getProperty("jdbc.postgresql.driverClass"));
+        dataSource.setUrl(environment.getProperty("jdbc.postgresql.url"));
+        dataSource.setUsername(environment.getProperty("jdbc.postgresql.username"));
+        dataSource.setPassword(environment.getProperty("jdbc.postgresql.password"));
+        return dataSource;
+    }
 
     @Bean
     public Message message(){
         return new Message();
+    }
+
+    @Bean
+    public Temp temp(){
+        return new Temp();
     }
 
 }
