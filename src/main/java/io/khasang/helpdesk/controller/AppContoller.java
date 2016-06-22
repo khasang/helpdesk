ef2d23ae07d2088bac37cdbc468183e35fb59393
Kuznetsov Sergey <azon.sk@gmail.com>
2016-06-22 20:50:06 +0300

    add spring security with encoding and receiving users from db

--- src/main/java/io/khasang/helpdesk/controller/AppContoller.java
+++ src/main/java/io/khasang/helpdesk/controller/AppContoller.java
@@ -4,9 +4,13 @@ import com.sun.org.apache.xpath.internal.operations.Mod;
 import io.khasang.helpdesk.model.CreateTable;
 import io.khasang.helpdesk.model.Message;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppContoller {
@@ -32,5 +36,19 @@ public class AppContoller {
     public String create(Model model){
         model.addAttribute("create", createTable.result());
         return "create";
+    }
+
+    @RequestMapping("/confidential/secure")
+    public String secure(Model model){
+        model.addAttribute("secure", "secure page with access encrypted");
+        return "secure";
+    }
+
+    @RequestMapping(value = {"/hello/{name}"}, method = RequestMethod.GET)
+    public ModelAndView hello(@PathVariable("name") String name) {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("helloPage");
+        model.addObject("crypt", new BCryptPasswordEncoder().encode(name));
+        return model;
     }
 }
