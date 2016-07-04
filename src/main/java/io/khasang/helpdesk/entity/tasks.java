be02e1352fb106116a57845f4d9ab768ba7147b2
mrak <deolands@gmail.com>
2016-07-04 21:59:37 +0300

    changes tasks.java

--- src/main/java/io/khasang/helpdesk/entity/tasks.java
+++ src/main/java/io/khasang/helpdesk/entity/tasks.java
@@ -1,19 +1,20 @@
 package io.khasang.helpdesk.entity;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "tasks")
 public class Tasks {
     private long id;
     private long user_id;
-    String comment;
-    date()
-    date_created
-    last_change
-    close_date
-    rates_id
-    state
+    private String comment;
+    private Date date;
+    private Date date_created;
+    private Date last_change;
+    private Date close_date;
+    private long rates_id;
+    private String state;
     }
 
 
