6edf7e3ce9520ed1b39ab8e300da7e5ab7cbfbd8
mrak <deolands@gmail.com>
2016-07-04 21:55:47 +0300

    add tasks.java to entity

--- src/main/java/io/khasang/helpdesk/entity/tasks.java
+++ src/main/java/io/khasang/helpdesk/entity/tasks.java
@@ -1,7 +1,19 @@
 package io.khasang.helpdesk.entity;
 
-/**
- *
- */
-public class tasks {
-}
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tasks")
+public class Tasks {
+    private long id;
+    private long user_id;
+    String comment;
+    date()
+    date_created
+    last_change
+    close_date
+    rates_id
+    state
+    }
+
+
