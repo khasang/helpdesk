3eefbbbd1ca7e39e61ee2a3a885b9c7a76042193
Alexander <2121alexander2121@gmail.com>
2016-07-04 22:45:07 +0300

    add new version

--- src/main/java/io/khasang/helpdesk/controller/DeskTableController.java
+++ src/main/java/io/khasang/helpdesk/controller/DeskTableController.java
@@ -1,4 +1,18 @@
 package io.khasang.helpdesk.controller;
 
+import io.khasang.helpdesk.entity.Table_in_task;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 public class DeskTableController {
+/*
+    @RequestMapping(value="/getItem/{id}", method= RequestMethod.GET)
+    public String getItem(@PathVariable("id") int id, Model model) {
+        Table_in_task item = itemService.getItem(id);
+        model.addAttribute("item", item);
+        return "buy";
+    }
+    */
 }
