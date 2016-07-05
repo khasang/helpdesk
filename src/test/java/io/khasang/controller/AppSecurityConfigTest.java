f21e9ac70795f607934f8070e7f8b902e54d87b3
George <geobarit@gmail.com>
2016-07-05 11:09:47 +0300

    try test security

--- src/test/java/io/khasang/controller/AppSecurityConfigTest.java
+++ src/test/java/io/khasang/controller/AppSecurityConfigTest.java
@@ -1,7 +1,26 @@
 package io.khasang.controller;
 
-/**
- * Created by Georgiy on 04.07.2016.
- */
+import io.khasang.helpdesk.config.AppContext;
+import io.khasang.helpdesk.config.AppSecurityConfig;
+import io.khasang.helpdesk.config.application.WebConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {AppContext.class, WebConfig.class, AppSecurityConfig.class})
 public class AppSecurityConfigTest {
+    @Autowired
+    AppSecurityConfig appSecurityConfig;
+
+    @Test
+    public void getCurrentUser(){
+        assertEquals();
+    }
 }
